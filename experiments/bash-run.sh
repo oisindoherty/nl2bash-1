ab50917d4626ab352317e0e51c0361faf6035465
Ubuntu <ubuntu@ip-172-31-15-246.us-east-2.compute.internal>
2018-04-27 15:56:05 +0000

    Reduce training time for experiment runs

--- experiments/bash-run.sh
+++ experiments/bash-run.sh
@@ -7,11 +7,12 @@ export PYTHONPATH=`pwd`'/..:'${PYTHONPATH}
 
 ARGS=${@:1}
 
+# num_epochs was 100, num_samples was 256
 python3 -m encoder_decoder.translate \
     --rnn_cell gru \
     --encoder_topology birnn \
-    --num_epochs 100 \
-    --num_samples 256 \
+    --num_epochs 1 \
+    --num_samples 32 \
     --variational_recurrent_dropout \
     --token_decoding_algorithm beam_search \
     --beam_size 100 \
